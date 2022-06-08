c926e8c76ea170976c3dbf48befbbf77d026681e
Michal Urbanski <michalurb8@gmail.com>
2022-06-08 14:51:46 +0200

    Fix formula use, need to test

--- R/ZUMensemble.R
+++ R/ZUMensemble.R
@@ -14,19 +14,26 @@
 #' @param pattr percent of all attributes to be sampled for one model
 #' @param ... named arguments passed to a single model
 ensemble <- function(algorithm, formula, dataset, mcount=100, pattr=0.6, ...) {
+  predicted_class <- as.character(formula[[2]])
   results <- list()
   for (i in 1:mcount) {
     attr_count <- max(1, floor(pattr * (ncol(dataset)-1)))
 
-    bootstrapped <- sample_n(dataset, nrow(dataset), replace=T)
+    bootstrapped <- dataset[sample(nrow(dataset), nrow(dataset), replace=TRUE),]
+    #without_class <- bootstrapped[,!(names(bootstrapped) == predicted_class)]
+    #attr_randomized <- sample(without_class, attr_count)
+    #attr_randomized[,predicted_class] <- bootstrapped[,predicted_class]
+    attr_names <- names(bootstrapped)
+    attr_names <- attr_names[!attr_names == predicted_class]
+    attr_names <- sample(attr_names, attr_count)
 
-    attr_randomized <- sample(bootstrapped[,-1], attr_count)
+    single_formula <- paste(predicted_class, " ~ ", paste(attr_names, collapse="+"), sep="")
+    single_formula <- as.formula(single_formula)
 
-    attr_randomized$class <- bootstrapped$class
-
-    new_model <- algorithm(formula, data=attr_randomized, ...)
+    new_model <- algorithm(formula=single_formula, data=dataset, ...)
     results[[i]] <- new_model
   }
+  results[[i+1]] <- predicted_class
   ensemble_model <- structure(results, class="EnsembleModel")
   return(ensemble_model)
 }
@@ -38,13 +45,14 @@ ensemble <- function(algorithm, formula, dataset, mcount=100, pattr=0.6, ...) {
 #' @param type if "class", returns a vector of classes, otherwise (default) a matrix of probabilities
 #' @param single_predict a method for prediction, default is "predict"
 predict.EnsembleModel <- function (ensemble_model, dataset, type="prob", single_predict=predict) {
-  modelClasses <- levels(dataset$class)
+  predicted_class <- ensemble_model[[length(ensemble_model)]]
+  modelClasses <- levels(as.factor(dataset[,predicted_class]))
   predictions <- matrix(0, nrow=nrow(dataset), ncol=length(modelClasses))
   colnames(predictions) <- modelClasses
 
   modelCount <- length(ensemble_model)
 
-  for (i in 1:length(ensemble_model)) {
+  for (i in 1:(length(ensemble_model)-1)) {
     newPrediction <- single_predict(ensemble_model[[i]], dataset, type="class")
 
     for (j in 1:length(newPrediction)) {
@@ -58,8 +66,9 @@ predict.EnsembleModel <- function (ensemble_model, dataset, type="prob", single_
       class_list[i] <- colnames(predictions)[which.max(predictions[i,])]
     }
     class_list <- as.factor(class_list)
-    levels(class_list) <- levels(dataset$class)
+    levels(class_list) <- modelClasses
     return(class_list)
   }
   return(predictions)
 }
+
